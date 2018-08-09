073f17db11fcdc940dc53b12079c936cecd5a14f
KazumaMuro <muu_kanata66@yahoo.ne.jp>
2018-08-09 17:31:10 +0900

    create question template

--- app/controllers/copper_controller.rb
+++ app/controllers/copper_controller.rb
@@ -1,4 +1,29 @@
 class CopperController < ApplicationController
   def home
   end
+
+  def question1
+  end
+
+  def question2
+  end
+
+  def question3
+  end
+
+  def question4
+  end
+
+  def explanation1
+  end
+
+  def explanation2
+  end
+
+  def explanation3
+  end
+
+  def explanation4
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'copper#home'
+  get "copper/question1" => "copper#question1"
+  get "copper/question2" => "copper#question2"
+  get "copper/question3" => "copper#question3"
+  get "copper/question4" => "copper#question4"
+  get "copper/explanation1" => "copper#explanation1"
+  get "copper/explanation2" => "copper#explanation2"
+  get "copper/explanation3" => "copper#explanation3"
+  get "copper/explanation4" => "copper#explanation4"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
